8fbb9cb5bd353b7de396bda192ad022d8e899152
emiliano <emilian.chiper.developer@gmail.com>
2024-07-17 12:32:07 +0300

    Create calculator object constructor and instantiate calculator object

--- src/js/script.js
+++ src/js/script.js
@@ -7,3 +7,21 @@ const [clearBtn, deleteBtn, equalsBtn] = [
 ];
 const operands = [...document.querySelectorAll('.operand')];
 const operators = [...document.querySelectorAll('.operators')];
+
+// Object constructor
+const Calculator = function (prev, curr) {
+  this.prev = prev;
+  this.curr = curr;
+  this.state = {
+    previousOperand: '',
+    currentOperand: '',
+    operations: undefined,
+  };
+  console.log(this);
+};
+
+// Instantiate object
+const calculator = new Calculator(
+  previousOperandElement,
+  currentOperandElement
+);
